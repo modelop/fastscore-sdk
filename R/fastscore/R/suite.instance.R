96726bb8b438f7183cb904f4bb71a720b2e04842
Matthew Mahowald <matthew.mahowald@opendatagroup.com>
2017-07-06 15:39:10 -0500

    Add instance base

---
+++ R/fastscore/R/suite.instance.R
@@ -0,0 +1,47 @@
+
+ActiveSensorBag <- setRefClass("ActiveSensorBag",
+    fields = list(
+        inst = "InstanceBase"
+    ),
+    methods = list(
+        ids = function(){
+            stop("Not implemented!") #TODO
+        },
+        get = function(tapid){
+            stop("Not implemented!") #TODO
+        },
+        del = function(tapid){
+            return(.self$inst$uninstall_sensor(tapid))
+        },
+    )
+)
+
+
+InstanceBase <- setRefClass("InstanceBase",
+    fields = list(
+        name="character",
+        api="character",
+        swg="list" #TODO: Change this
+    ),
+    methods = list(
+        active_sensors = function(){
+            stop("Not implemented!") #TODO
+        },
+        tapping_points = function(){
+            return(.self$swg$active_sensor_available(.self$name))
+        },
+        check_health = function(){
+            return(.self$swg$health_get(.self$name))
+        },
+        get_swagger = function(){
+            return(.self$swg$swagger_get(.self$name))
+        },
+        install_sensor = function(sensor){
+            return(.self$swg$active_sensor_attach(.self$name, sensor$desc))
+        }
+        uninstall_sensor = function(tapid){
+            return(.self$swg$active_sensor_detach(.self$name, tapid))
+        }
+    )
+
+)
