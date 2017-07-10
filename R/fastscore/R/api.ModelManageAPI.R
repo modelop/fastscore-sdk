1df41c7283d178daf2a6d3f33fe49da827f1ec83
Matthew Mahowald <matthew.mahowald@opendatagroup.com>
2017-07-10 17:30:54 -0500

    Add ModelManageAPI shell

---
+++ R/fastscore/R/api.ModelManageAPI.R
@@ -0,0 +1,52 @@
+ModelManageAPI <- setRefClass("ModelManageAPI",
+    contains="APIBase",
+    fields=list(),
+    methods=list(
+        model_get_with_http_info=function(instance, name){
+
+        },
+        model_put_with_http_info=function(instance, name, body, content_type){
+
+        },
+        model_delete=function(instance, name){
+
+        },
+        schema_put_with_http_info = function(instance, name, body){
+
+        },
+        schema_list=function(instance){
+
+        },
+        schema_get=function(instance, name){
+
+        },
+        schema_delete=function(instance, name){
+
+        },
+        stream_put_with_http_info = function(instance, name, body){
+
+        },
+        stream_list=function(instance){
+
+        },
+        stream_get=function(instance, name){
+
+        },
+        stream_delete=function(instance, name){
+
+        },
+        sensor_put_with_http_info = function(instance, name, body){
+
+        },
+        sensor_list=function(instance){
+
+        },
+        sensor_get=function(instance, name){
+
+        },
+        sensor_delete=function(instance, name){
+
+        },
+
+    )
+)
