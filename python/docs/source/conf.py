a7d6668bbcee2fbb08acf88765c4948f4f99bc2a
Matthew Mahowald <matthew.mahowald@opendatagroup.com>
2017-12-08 09:32:57 -0600

    Add sphinxtogithub to conf.py

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'sphinxtogithub'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
