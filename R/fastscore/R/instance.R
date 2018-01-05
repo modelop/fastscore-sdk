50d03d9e8479ed71273b56985c41f88a84d298bf
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-01-05 11:49:51 -0800

    Added $tapping_points(), $check_health()
    Added:
    (1) InstanceBase$tapping_points(...)
    (2) InstanceBase$check_health(...)

--- R/fastscore/R/instance.R
+++ R/fastscore/R/instance.R
@@ -4,48 +4,86 @@
 # from ..v1.rest import ApiException
 # from ..v2.models import ActiveSensorInfo
 
+# Where (?) to put this:
+# httr::set_config(httr::config(ssl_verifypeer = FALSE))
+
+
 InstanceBase <- R6::R6Class("InstanceBase",
     public = list(
 
       name = NULL,
       api = NULL,
-      swg = NULL,
+      # swg = NULL,
+      # swg2 = NULL, # Maxim: N/A for R-SDK
 
       initialize = function(name = NA, api = NA, swg = NA){
         self$name <- name
         self$api <- api
-        self$swg <- swg
+        # self$swg  <- swg
+        # self$swg2 <- swg2
       },
 
       # @property (???)
-
-      active_sensors = function(self){
+      active_sensors = function(){
+        #' Currently installed sensors indexed by id
+        #'
+        #' > engine <- Connect$lookup('engine')
+        #' > names(engine$active_sensors())
+        #' > x <- engine$active_sensors["sensor"]
+        #' > str(x)
 
         tryCatch(
           {
-            d <- list()
-            for(i in victory$active_sensor_list(self$name)){
-              d[[paste(i)]] <- i
-              }
-            d
+             d <- list()
+             for(i in self$active_sensor_list(self$name)){
+               d[[paste(i)]] <- i
+               }
+             d
             },
-        error = function(e) FastScoreError$new(
-          message = "Unable to retrieve active sensors.",
-          caused_by = e$message)$error_string()
-        # warning = FastScoreError$new(message = "...", caused_by = "..."),
-        # message = FastScoreError$new(message = "...", caused_by = "...")
-        )
+          error = function(e) FastScoreError$new(
+            message = "Unable to retrieve active sensors.",
+            caused_by = e$message
+            )$error_string()
+          )
       },
 
       # @property (???)
-      tapping_points = function(self){},
+      tapping_points = function(){
+        # List of tapping points supported by the instance
+        #
+        # > mod_man$tapping_points()
 
-      check_health = function(self){},
+        tryCatch(
+          self$tapping_points(self$name),
 
-      get_swagger = function(self){},
+          error = function(e) FastScoreError$new(
+            message = "Unable to list tapping points.",
+            caused_by = e$message
+            )$error_string()
+        )
+      },
+      # calls  v2.yaml method: tapping_points(...)
 
-      install_sensor = function(self, sensor){},
+      check_health = function(){
+        # Retrieves version information from the instance.
+        # A successful reply indicates that the instance is healthy.
+        #
+        # > Connect$check_health()
 
-      uninstall_sensor = function(self, tapid){}
+        tryCatch(
+          self$health_get(self$name),
+
+          error = function(e) FastScoreError$new(
+            message = "Unable to retrieve instance info.",
+            caused_by = e$message
+            )$error_string()
+        )
+      },
+
+      get_swagger = function(){},
+
+      install_sensor = function(sensor){},
+
+      uninstall_sensor = function(tapid){}
     )
 )
