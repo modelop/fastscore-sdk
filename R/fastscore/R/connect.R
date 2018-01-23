5eac2ad272343637b521ead66b5c35138746fd08
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-01-23 13:50:29 -0800

    Create instance, class, and inheritance

--- R/fastscore/R/connect.R
+++ R/fastscore/R/connect.R
@@ -13,10 +13,45 @@ unset_auth_cookie <- function(client1, client2){
   client2$cookie <- NULL
 }
 
-Connect <- function(InstanceBase){
+Connect <- R6::R6Class("Connect",
 
+    inherit = ConnectApi, # swagger twin
+    public = list(
+      proxy_prefix = NULL,
+      api_client = NULL,
+      auth_secret = NULL,
 
+      initialize = function(proxy_prefix, api_client, auth_secret = NA){
 
+        if(!grepl("://", proxy_prefix)){
+          FastScoreError$new(
+            message = "Proxy prefix must be an URL, e.g. https://dashboard:8000")$error_string()
+          }
+        if(!grepl("https:", proxy_prefix)){
+          FastScoreError$new(
+            message = "Proxy prefix must use HTTPS scheme")$error_string()
+        }
 
-}
+        self$proxy_prefix <- proxy_prefix
+        self$auth_secret <- auth_secret
+
+          if (!missing(api_client)) {
+            self$api_client <- api_client
+          }
+          else {
+            self$api_client <- InstanceBase$new() # fastscore parent
+          }
+      },
+
+      fleet = function(){
+        tryCatch(
+          self$connect_get(self$name),
+          error = function(e) FastScoreError$new(
+            message = "Cannot retrieve fleet info.",
+            caused_by = e$message
+          )$error_string()
+        )
+      }
+    )
+)
 
