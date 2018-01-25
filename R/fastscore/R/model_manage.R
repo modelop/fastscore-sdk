5b002c33b86f342343181973dd10039c96b42e8e
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-01-25 14:41:35 -0800

    add ModelManage class

--- R/fastscore/R/model_manage.R
+++ R/fastscore/R/model_manage.R
@@ -1,1 +1,16 @@
 # model_manage.py
+
+ModelManage <- R6::R6Class("ModelManage",
+    inherit = ModelManageApi, # swagger twin
+    public = list(
+      apiClient = NA,
+      basePath = NA,
+      initialize = function(apiClient, basePath = NA){
+        self$apiClient <- apiClient # fastscore parent
+        self$basePath <- apiClient$basePath
+      }
+
+    )
+
+
+)
