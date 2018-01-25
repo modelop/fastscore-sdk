763c73c84ec716a1d369aede52811b5d26bc439c
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-01-25 14:06:02 -0800

    Add stopifnot(...), require apiClient

--- R/fastscore/R/connect.R
+++ R/fastscore/R/connect.R
@@ -1,7 +1,4 @@
 # library(swagger)
-
-httr::set_config(httr::config(ssl_verifypeer = FALSE)) # global ignore-self-certify config
-
 set_auth_cookie <- function(auth_secret, client1, client2){
   cookie <- paste0(connect$sid, auth_secret)
   client1$cookie <- cookie
@@ -17,53 +14,35 @@ Connect <- R6::R6Class("Connect",
 
     inherit = ConnectApi, # swagger twin
     public = list(
-      proxy_prefix = NULL,
       apiClient = NA,
       basePath = NA,
       auth_secret = NA,
 
+      initialize = function(apiClient, basePath = NA, auth_secret = NA){
 
-      initialize = function(proxy_prefix, apiClient = NA, basePath = NA, auth_secret = NA){
-
-        if(!grepl("://", proxy_prefix)){
-          FastScoreError$new(
-            message = "Proxy prefix must be an URL, e.g. https://dashboard:8000")$error_string()
-          }
-        if(!grepl("https:", proxy_prefix)){
-          FastScoreError$new(
-            message = "Proxy prefix must use HTTPS scheme")$error_string()
-        }
-
-        self$proxy_prefix <- proxy_prefix
         self$auth_secret <- auth_secret
-
-        if (!missing(apiClient)) {
-          self$apiClient <- apiClient # fastscore parent, from argument
-        } else {
-          self$apiClient <- InstanceBase$new() # instantiate fastscore parent
-        }
-
-        if (!missing(basePath)) {
-          self$basePath <- basePath
-        } else {
-          if (!missing(apiClient)){
-            self$basePath <- apiClient$basePath
-          } else {
-            self$basePath <- self$apiClient$basePath
-          }
-          }
+        self$apiClient <- apiClient # fastscore parent
+        self$basePath <- apiClient$basePath
 
       },
 
-      fleet = function(){
+      get = function(){}
+
+      lookup = function(){}
+
+      fleet = function(instance){
         tryCatch(
-          self$connect_get(self$name),
+          self$connect_get(instance),
           error = function(e) FastScoreError$new(
             message = "Cannot retrieve fleet info.",
             caused_by = e$message
           )$error_string()
         )
       }
+
+
+
+
     )
 )
 
