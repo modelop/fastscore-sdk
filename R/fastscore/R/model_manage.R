0f14d97e908be1c0b19e4e3d96de7399ef7c2813
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-02-09 14:18:47 -0800

    add class methods to override inherited methods

--- R/fastscore/R/model_manage.R
+++ R/fastscore/R/model_manage.R
@@ -1,4 +1,14 @@
-# model_manage.py
+#' RC implementation of Queue
+#'
+#' A simple example of what is possible using roxygen 4.1.1
+#'
+#' @field queue a list of items in the queue
+#' @examples
+#' (qrc <- QueueRC(5, 6, "foo"))
+#' qrc$add("add something")
+#' qrc$add("and something else")
+#' qrc$remove()
+#' @export
 
 ModelManage <- R6::R6Class("ModelManage",
     inherit = ModelManageApi, # swagger twin
@@ -8,9 +18,78 @@ ModelManage <- R6::R6Class("ModelManage",
       initialize = function(apiClient, basePath = NA){
         self$apiClient <- apiClient # fastscore parent
         self$basePath <- apiClient$basePath
+      },
+
+      # overwrite swagger::ModelManageApi$new()$model_list()
+      model_list = function(instance, return, ...){
+        args <- list(...)
+        queryParams <- list()
+        headerParams <- character()
+
+        if (!missing(`return`)) {
+          queryParams['return'] <- return
+        }
+
+        urlPath <- "/{instance}/1/model"
+        if (!missing(`instance`)) {
+          urlPath <- gsub(paste0("\\{", "instance", "\\}"), `instance`, urlPath)
+        }
+
+        resp <- self$apiClient$callApi(url = paste0(self$apiClient$basePath, urlPath),
+                                       method = "GET",
+                                       queryParams = queryParams,
+                                       headerParams = headerParams,
+                                       body = body,
+                                       ...)
+
+        if (httr::status_code(resp) >= 200 && httr::status_code(resp) <= 299) {
+          # returnObject <- TODO_OBJECT_MAPPING$new()
+          # result <- returnObject$fromJSON(httr::content(resp, "text", encoding = "UTF-8"))
+          result <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"))
+          # Response$new(returnObject, resp)
+          Response$new(result, resp)
+        } else if (httr::status_code(resp) >= 400 && httr::status_code(resp) <= 499) {
+          Response$new("API client error", resp)
+        } else if (httr::status_code(resp) >= 500 && httr::status_code(resp) <= 599) {
+          Response$new("API server error", resp)
+        }
+
+      },
+
+      # overwrite swagger::M_ M_ A_ $new()$model_get
+      model_get = function(instance, model, ...){
+        args <- list(...)
+        queryParams <- list()
+        headerParams <- character()
+
+        urlPath <- "/{instance}/1/model/{model}"
+        if (!missing(`instance`)) {
+          urlPath <- gsub(paste0("\\{", "instance", "\\}"), `instance`, urlPath)
+        }
+
+        if (!missing(`model`)) {
+          urlPath <- gsub(paste0("\\{", "model", "\\}"), `model`, urlPath)
+        }
+
+        resp <- self$apiClient$callApi(url = paste0(self$apiClient$basePath, urlPath),
+                                       method = "GET",
+                                       queryParams = queryParams,
+                                       headerParams = headerParams,
+                                       body = body,
+                                       ...)
+
+        if (httr::status_code(resp) >= 200 && httr::status_code(resp) <= 299) {
+          # returnObject <- Character$new()
+          # result <- returnObject$fromJSON(httr::content(resp, "text", encoding = "UTF-8"))
+          result <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"))
+          # Response$new(returnObject, resp)
+          Response$new(result, resp)
+        } else if (httr::status_code(resp) >= 400 && httr::status_code(resp) <= 499) {
+          Response$new("API client error", resp)
+        } else if (httr::status_code(resp) >= 500 && httr::status_code(resp) <= 599) {
+          Response$new("API server error", resp)
+        }
+
       }
-
     )
-
-
 )
