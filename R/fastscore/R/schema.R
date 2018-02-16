059b165acf327466d168ac4ede51f410a843c6dc
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-02-16 12:49:08 -0800

    add schema class

--- R/fastscore/R/schema.R
+++ R/fastscore/R/schema.R
@@ -1,1 +1,13 @@
-# schema.py
+Schema <- R6::R6Class("Schema",
+    public = list(
+      name = NA,
+      source = NA,
+      model_manage = NA,
+
+      initialize = function(name, source = NA, model_manage = NA){
+        self$name <- name
+        self$source <- source
+        self$model_manage <- model_manage
+      }
+      )
+    )
