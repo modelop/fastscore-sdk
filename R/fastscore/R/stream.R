0747336b0f5bdc5939f517071632e92e13108da7
Chris Comiskey <32367409+ChrisComiskey@users.noreply.github.com>
2018-02-20 14:03:37 -0800

    Add R6::Stream(...)

--- R/fastscore/R/stream.R
+++ R/fastscore/R/stream.R
@@ -1,1 +1,12 @@
-# stream.py
+Stream <- R6::R6Class("Stream",
+                     public = list(
+                       name = NA,
+                       source = NA,
+                       model_manage = NA,
+                       initialize = function(name, source = NA, model_manage = NA){
+                         self$name <- name
+                         self$source <- source
+                         self$model_manage <- model_manage
+                       }
+                     )
+)
